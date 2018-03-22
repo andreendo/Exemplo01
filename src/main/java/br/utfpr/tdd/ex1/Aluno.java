e86200617069c70023aa29d7ac7ca124221b139f
Anderson <anderson.soares@alunos.utfpr.edu.br>
2018-03-22 20:00:54 -0300

    Teste RAA salvando Sucesso

--- src/main/java/br/utfpr/tdd/ex1/Aluno.java
+++ src/main/java/br/utfpr/tdd/ex1/Aluno.java
@@ -44,7 +44,8 @@ public class Aluno {
     double getNF() {
         if(notaRAA < 0)
             return (notaProjeto + getNAP()) / 2.0;
-        
+        if((notaProjeto + getNAP() + notaRAA) / 3.0 > 6.0)
+            return 6.0;
         return (notaProjeto + getNAP() + notaRAA) / 3.0;
     }
 
