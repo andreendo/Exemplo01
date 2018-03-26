db1bed78c183705addff073f5ad0a910e792b1e2
GabrieldeCarli <gabrielcarli@alunos.utfpr.edu.br>
2018-03-26 16:01:51 -0300

    Commit
    Commit da Atividade 05 - Falta

--- src/test/java/br/utfpr/tdd/ex1/AlunoTest.java
+++ src/test/java/br/utfpr/tdd/ex1/AlunoTest.java
@@ -1,6 +1,5 @@
 package br.utfpr.tdd.ex1;
 
-import 
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -87,5 +86,22 @@ public class AlunoTest {
         
         assertEquals(6.0 , aluno.getNF(), 0.01);
     }
-    
+    @Test
+     public void testArredondarNotaFinal() {
+        Aluno aluno = new Aluno("RA", "Joao Da Silva");
+        
+        //6.3 media
+        aluno.addNotaAtividadePratica(6.0);
+        aluno.addNotaAtividadePratica(6.0);
+        aluno.addNotaAtividadePratica(6.0);
+        aluno.addNotaAtividadePratica(6.0);
+        aluno.addNotaAtividadePratica(6.0);
+        aluno.addNotaAtividadePratica(6.0);
+        double mediaRAA = (6.0 + 6.0 + 6.0 + 6.0 + 6.0)/6.0;
+        
+        aluno.setNotaProjeto(4.0);
+        aluno.setNotaRAA(7.8);
+        
+        assertEquals(6.0 , aluno.getNF(), 0.01);
+    }
 }
